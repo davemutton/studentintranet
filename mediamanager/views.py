1faf9b39fcd76d07321348d224369adf3a3f9d2b
Dave Mutton <Dave.mutton490@schools.sa.edu.au>
2015-02-17 22:26:18 +1030

    attempted to add dropzone (not complete)

--- mediamanager/views.py
+++ mediamanager/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, render_to_response
 from django.views.generic import CreateView
-from mediamanager.models import LearningObject
+from mediamanager.models import LearningObject, FileResource
+from forms import UploadFileForm
 from mediamanager.forms import FileResourceForm
+from django.template import RequestContext
 # Create your views here.
 
 class createLearningobject(CreateView):
@@ -31,3 +33,17 @@ def FileResourceFormView(request):
 	return render(request, 'mediamanager/file_upload.html', {'form': form,})
 
 
+def test(request):
+    if request.method == 'POST':
+        form = UploadFileForm(request.POST, request.FILES)
+        print "request.FILES"
+        if form.is_valid():
+            new_file = UploadFile(file = request.FILES['file'])
+            new_file.save()
+ 
+            return HttpResponseRedirect(reverse('main:home'))
+    else:
+        form = UploadFileForm()
+ 
+    data = {'form': form}
+    return render_to_response('mediamanager/test.html', data, context_instance=RequestContext(request))
